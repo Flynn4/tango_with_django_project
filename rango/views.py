39bd3a32a4fba6b03c654b8e2833f643fc731575
Flynn4 <hmf245388320@qq.com>
2020-02-03 10:39:20 +0000

    Working on Chapter 6 Page.100

--- rango/views.py
+++ rango/views.py
@@ -1,24 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
-def index(request): 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    
+
+def index(request):
+    # Query the database for a list of ALL categories currently stored.
+    # Order the categories by the number of likes in descending order.
+    # Retrieve the top 5 only -- or all if less than 5.
+    # Place the list in our context_dict dictionary (with our boldmessage!)
+    # that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context=context_dict)
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
 
-def about(request): 
+
+def about(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    #context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    
+    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/about.html', )
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")
+    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")

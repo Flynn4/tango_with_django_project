8e160c410f478d0f4a2978c947a21f9815e7bbc5
Flynn4 <hmf245388320@qq.com>
2020-02-03 03:57:12 +0000

    Chapter 5 Finshed

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    def __str__(self):
+        return self.name
     
 def __str__(self):
     return self.name
@@ -12,7 +20,6 @@ class Page(models.Model):
     url = models.URLField()
     views = models.IntegerField(default=0)
     
-def __str__(self):
-    return self.title
-
+    def __str__(self):
+        return self.title
 

e06e5ae67d1991210bbfaf8989c486fc6103f561
Flynn4 <hmf245388320@qq.com>
2020-02-03 21:04:04 +0000

    Chapter 8 Finished, Test all passed

--- rango/views.py
+++ rango/views.py
@@ -30,15 +30,11 @@ def index(request):
 
 
 def about(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    # context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/about.html', )
-    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>.")
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 
 def show_category(request, category_name_slug):
